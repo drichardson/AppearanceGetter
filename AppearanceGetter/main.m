00339e64153a7e1ffa9d4478037a43f14a264bc3
Doug Richardson <git@rekt.email>
2021-08-18 14:07:07 -0700

    Initial Commit

---
+++ AppearanceGetter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppearanceGetter
+//
+//  Created by Douglas Richardson on 8/18/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
